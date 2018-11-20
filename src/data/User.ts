6bbbb1a4046a9922e1eeebfde9a36dd0b75a3a7f
Joseph Onifade <thejosephonifade@gmail.com>
2018-11-20 11:09:25 -0500

    edited to dynamically assign credit year

--- src/data/User.ts
+++ src/data/User.ts
@@ -1,6 +1,7 @@
 import {CompletedCourse} from "./CompletedCourse";
 import {catalog} from "./Course";
 import {rules} from "./Rule";
+import { CS_PROGRAM } from "./CSProgram";
 
 /**
  * The year name of the user
@@ -29,7 +30,21 @@ export class User {
     ) {
         this.semestersRemaining = semestersRemaining;
         this.year = year;
-        this.creditYear = creditYear;
+        
+        // Dynamically generate the credit classification
+        let credits = 0;
+        for (let course of coursesTaken){
+            credits += 3; // TODO: This needs to be changed to the actually credits of the class.
+                          // Assuming 3 credits for each class for the sake of demoing
+        }
+        this.creditYear = Year.Senior; // TODO: override all files that call the constructor to accoutn for this
+        if(credits < 30)
+            this.creditYear = Year.Freshman;
+        else if(credits < 60)
+            this.creditYear = Year.Sophomore;
+        else if(credits < 90)
+            this.creditYear = Year.Junior;
+
         this.coursesTaken = coursesTaken;
     }
 
