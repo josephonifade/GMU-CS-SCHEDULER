7af0fa07cc81b3bdce867cebfcb615afef278145
Adilet Uspeev <adius9@gmail.com>
2018-11-25 21:29:59 -0500

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-console.log("Hello world!!! :)");
 
-$("#example").text("Hello from the ts file!!. You can edit stuff here!!");
+import { initializeUI } from './ui/main'
+
+interface Semester {
+  year: number
+  courses: object[]
+}
+
+interface Store {
+  semesters: Semester[]
+}
+
+const DataStore = {
+
+} as Store
+
+// Add this to current catalog
+addEventListener('semester-added', (event) => {
+  const semester = event.detail
+  console.log(semester)
+  // Update current catalog
+})
+
+// Fire this in generate schedules once finished creating a semester
+dispatchEvent(new CustomEvent('semester-added', {
+  detail: { year: 1201 }
+}))
+
+initializeUI()
