2d2c6f7077a358fa2500fb81e962444f7b23574e
Elvis Ly <ly.elvis@yahoo.com>
2018-11-28 23:49:42 -0500

    update to file courseID test

--- src/tests/data/CourseID.test.ts
+++ src/tests/data/CourseID.test.ts
@@ -4,5 +4,24 @@ import {CourseID} from "../../data/CourseID"
 let temp = new CourseID('CS', 321);
 
 test("Test if the user has entered correct course format", () => {
-    expect(temp.toString()).toEqual();
-}
+    expect(temp.toString()).toEqual('CS 321');
+});
+
+test("testing the department of the course", () => {
+    expect(temp.department == "CS");
+});
+
+test("testing the course number", () => {
+    expect(temp.courseNumber == 321);
+});
+
+/* not sure why this one is giving me an error
+test("test the fromCourseName function", () => {
+    expect(temp.fromCourseName("CS 471").toEqual(new CourseID('CS', 471)));
+});
+*/
+
+test("test the equals function", () => {
+    expect(temp.equals(new CourseID('CS', 321)));
+});
+
