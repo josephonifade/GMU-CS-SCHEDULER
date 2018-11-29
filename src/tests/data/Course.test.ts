c4222882758352b8b066bd6f4f463790ccf1e83f
Elvis Ly <ly.elvis@yahoo.com>
2018-11-29 00:21:47 -0500

    still working on it

---
+++ src/tests/data/Course.test.ts
@@ -0,0 +1,10 @@
+import {CompletedCourse} from "../../data/CompletedCourse";
+import {CourseID} from "../../data/CourseID";
+import {Grade} from "../../data/Grade";
+import {Prereq} from "../../data/Prereq";
+import {CREDIT_RULE, TOTAL_CREDIT_REQUIREMENT} from "../../data/Rule";
+import {User, Year} from "../../data/User";
+
+test("testing the features of course function", () => {
+
+});
