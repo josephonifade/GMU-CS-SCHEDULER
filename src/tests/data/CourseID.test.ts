ccb77bc9a03cf720d8f60e202da0383c530ddc92
Elvis Ly <ly.elvis@yahoo.com>
2018-11-28 19:58:23 -0500

    courseID.test FILE

---
+++ src/tests/data/CourseID.test.ts
@@ -0,0 +1,8 @@
+import {RequirementOption} from "../../data/CSProgram";
+import {CourseID} from "../../data/CourseID"
+
+let temp = new CourseID('CS', 321);
+
+test("Test if the user has entered correct course format", () => {
+    expect(temp.toString()).toEqual();
+}
